35a5a1ad0030ce162b4352f1ef9001c8054a61ef
Jason Sebastian Flynn <J.S.Flynn@outlook.com>
2019-03-02 15:18:41 +0000

    added string concatonation

--- fundamentals.cpp
+++ fundamentals.cpp
@@ -5,7 +5,9 @@
 
 using namespace std; 
 
-void message() ; 
+void message() ; // declared before main so it can be used 
+void ifStatment(int) ; // declared before main with input types 
+void specificHello(string) ;
 
 //we have two functions , message and getNumber , one is defined befor main, and the other after. 
 // both can be called in main. the reason this is the cas eis that getNumber is declared and defined before main. message is declared befor, but dfined after. 
@@ -17,15 +19,39 @@ int getNumber(int baseNumber, int multiplyer){
 
 int main(){ 
 
-    cout << getNumber(5, 5) << endl  ; 
+    message() ; // calls a function that prints to the console 
+    
+    cout << getNumber(5, 2) << endl  ;  // arbritrarily multiplying numbers to show how we can send variables to a function 
 
-    message() ; 
+    ifStatment(getNumber(5, 2)) ;  //using the output of one function to pass value to another function 
+    
+    specificHello("ThoXIII") ; 
+
     return 0 ; 
 }
 
-
+//takes no value simply gets the function to perform an action 
 void message(){
 
     cout << "Hello World!" << endl ; 
 
+}
+
+//takes an int value for evaluation in if statmenr, sows basic iff functionality 
+// can use logical opirators like &&, ||, >, <, ==, <=, >=, ! 
+void ifStatment(int num){
+
+    if(num < 5){
+        cout << "Not a ver bug number that mate!" << endl ; 
+    }else if(num > 10){
+        cout << "Number is bigger than ten, no need for that!" << endl ; 
+    }else{
+        cout << "Not 100% sure, you could have just done an evalute to see if it was equal. now I have to guess!" << endl ; 
+    }
+}
+
+//takes a string and concatinats it to the output 
+void specificHello(string name){
+
+    cout << "Thanks " + name + " for all the things you have done" << endl ; 
 }
