07b78c14573784913265aa65aed65c6bb45b95b4
Jason Sebastian Flynn <J.S.Flynn@outlook.com>
2019-03-02 14:59:05 +0000

    jdeclarations before main, defanition after

--- fundamentals.cpp
+++ fundamentals.cpp
@@ -5,8 +5,26 @@
 
 using namespace std; 
 
-int main(){
+void message() ; 
+//compleatly forgot, code compilation is not like java. the function must be defined befor it can be called
+//i.e if you make a function below main and try to call it, main will not lie it.   
+int getNumber(int baseNumber, int multiplyer){
 
-    cout << "Hello World!" << endl  ; 
+        int sum = baseNumber * multiplyer ; 
+        return sum ;
+}
+
+int main(){ 
+
+    cout << getNumber(5, 5) << endl  ; 
+
+    message() ; 
     return 0 ; 
+}
+
+
+void message(){
+
+    cout << "Hello World!" << endl ; 
+
 }
